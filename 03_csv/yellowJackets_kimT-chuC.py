dbd6832033108873e8cc5709434766872b96f730
Taejoon Kim <tkim00@stuy.edu>
2019-09-16 21:22:18 -0400

    fixed my code with the help of my partner; code should be working now

--- 03_csv/yellowJackets_kimT-chuC.py
+++ 03_csv/yellowJackets_kimT-chuC.py
@@ -4,21 +4,23 @@ import random
 occupations = {}
 
 def turnIntoDict(file):
-    with open(file, r) as csvfile:
+    with open(file, 'r') as csvfile:
         readCSV = csv.reader(csvfile)
-        readCSV.next() #skip the first row cuz it's unnecessary
+        next(readCSV) #skip the first row cuz it's unnecessary
         for row in readCSV:
-            occupations.update(row[0] = row[1]) #turn csv into a dictionary
+            occupations[row[0]] = row[1] #turn csv into a dictionary
 
 
 def occupationPicker(dic):
-    t = 0
+    t = 0.0
     x = random.random() * 99.8 #generating a random number between 0 and 99.8
     for key in list(dic):
-        t += dic[key]
+        #print(dic[key])
+        t += float(dic[key])
         if x < t:
             return key
 
 turnIntoDict("occupations.csv")
-occupations.pop('total')
-occupationPicker(occupations)
+#print(occupations)
+occupations.pop('Total')
+print(occupationPicker(occupations))
