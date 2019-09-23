54377ae831b35b8c63da72e182eb61daa4fdd706
Taejoon Kim <tkim00@stuy.edu>
2019-09-22 21:12:21 -0400

    actually changed something

--- 08_app0/app.py
+++ 08_app0/app.py
@@ -1,5 +1,5 @@
-#Clyde "Thluffy" Sinclair
-#SoftDev1 pd0
+#Taejoon Kim
+#SoftDev1 pd9
 #demo -- My First Flask App
 #2019-09-17t
 
@@ -10,6 +10,17 @@ app = Flask(__name__) #create instance of class Flask
 def hello_world():
     print(__name__) #where will this go?
     return "No hablo queso!"
+
+@app.route("/english") #assign following fxn to run when root route requested
+def hello_world():
+    print(__name__) #where will this go?
+    return "I don't speak cheese!"
+
+@app.route("/japanese") #assign following fxn to run when root route requested
+def hello_world():
+    print(__name__) #where will this go?
+    return "cheezu shaberarenaidesu!"
+
 
 if __name__ == "__main__":
     app.debug = True
