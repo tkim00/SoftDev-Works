76bad51f2c79d0af15143bdc6c06267fa67e976f
Taejoon Kim <tkim00@stuy.edu>
2019-09-27 00:15:59 -0400

    finished with making two page flask app

--- 12_form/app.py
+++ 12_form/app.py
@@ -1,17 +1,27 @@
+#Biraj Chowdhury and Taejoon Kim Team Excelsius
+#SoftDev1 pd 9
+#K12 <Echo Echo Echo/Html forms/Using the request methods and html forms>
+#2019 09 26
+
 from flask import Flask, render_template, request
 app = Flask(__name__)
-#@app.route("/")
 
+@app.route("/")
+def homepage():
+    return render_template("home.html")
 
 @app.route("/auth")
 def render():
-    render_template("foo.html")
+    #render_template("foo.html")
     print(app)
     print("//////////////}}}}}}}}}}}}}}}}}}}}}}}}")
-    print(request)
-    print("################################################")
     print(request.args)
-    return "this is foo"
+    print("################################################")
+    if "yourname" in request.args:
+        yourname = request.args["yourname"]
+    else:
+        yourname = "User"
+    return render_template("foo.html",name=yourname)
 
 
 if __name__ == "__main__":
