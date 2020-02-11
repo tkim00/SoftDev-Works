37a7b27e4773e28c73af22ed38c3c112ff096169
Taejoon Kim <tkim00@stuy.edu>
2020-02-11 14:51:09 -0500

    started working on 05_canvas

---
+++ 05_canvas/clickdraw.js
@@ -0,0 +1,24 @@
+const c = document.getElementById('slate');
+const ctx = c.getContext("2d");
+ctx.fillStyle = "#ff0000";
+let state = "rect";
+document.getElementById("clear").addEventListener("click", _=>clear());
+const clear = _=> {
+  ctx.clearRect(0,0,c.width,c.height);
+}
+c.addEventListener("click", e=>{
+  if (state === "rect") {
+    ctx.fillRect(e.pageX, e.pageY, 100, 200);
+  }
+  if (state === "circle") {
+    ctx.fillEllipse
+  }
+});
+
+
+/*
+var c = document.getElementById('slate');
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(50,50,100,200)
+*/
